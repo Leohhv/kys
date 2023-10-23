5f624b48fd529fa371a3cf37baf01e74e546c6ff
Leohhv <146135744+Leohhv@users.noreply.github.com>
2023-10-23 15:37:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include <string>
 int main() {
-std::cout << "hello world";
-  return 0;
+std::string a= "1234";
+std::string b = a.substring(0,2), x= a.begin(), y=a.end();
+
+int ab = std::stoi(b)+std::stoi(a);
+std::string c=std::to_string(ab)}
+
+int len=a.length();//4
+
+return 0;
 }
